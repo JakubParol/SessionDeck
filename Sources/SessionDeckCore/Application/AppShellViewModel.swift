760e805f57f1df5a8725121953aefe7361f5ffcb
Jakub Parol <258680984+JakubParol@users.noreply.github.com>
2026-06-04 12:29:04 +0200

    SDECK-147 Add app shell source discovery state

--- Sources/SessionDeckCore/Application/AppShellViewModel.swift
+++ Sources/SessionDeckCore/Application/AppShellViewModel.swift
@@ -1,8 +1,50 @@
+public enum AppShellRefreshState: Equatable, Sendable {
+    case idle
+    case refreshing
+    case failed(String)
+}
+
+public struct AppShellSourceDiscoverySummary: Equatable, Sendable {
+    public static let placeholder = AppShellSourceDiscoverySummary(
+        configuredSourceCount: 0,
+        availableSourceCount: 0,
+        candidateFileCount: nil,
+        warningCount: 0,
+        errorCount: 0,
+        statusMessage: "Source discovery has not run yet."
+    )
+
+    public let configuredSourceCount: Int
+    public let availableSourceCount: Int
+    public let candidateFileCount: Int?
+    public let warningCount: Int
+    public let errorCount: Int
+    public let statusMessage: String
+
+    public init(
+        configuredSourceCount: Int,
+        availableSourceCount: Int,
+        candidateFileCount: Int?,
+        warningCount: Int,
+        errorCount: Int,
+        statusMessage: String
+    ) {
+        self.configuredSourceCount = configuredSourceCount
+        self.availableSourceCount = availableSourceCount
+        self.candidateFileCount = candidateFileCount
+        self.warningCount = warningCount
+        self.errorCount = errorCount
+        self.statusMessage = statusMessage
+    }
+}
+
 public struct AppShellViewModel: Equatable, Sendable {
     public let title: String
     public let subtitle: String
     public let statusMessage: String
     public let configuredSourceCount: Int
+    public let sourceDiscoverySummary: AppShellSourceDiscoverySummary
+    public let refreshState: AppShellRefreshState
     public let safetyPolicy: LaunchSafetyPolicy
 
     public init(
@@ -10,12 +52,16 @@ public struct AppShellViewModel: Equatable, Sendable {
         subtitle: String,
         statusMessage: String,
         configuredSourceCount: Int,
+        sourceDiscoverySummary: AppShellSourceDiscoverySummary = .placeholder,
+        refreshState: AppShellRefreshState = .idle,
         safetyPolicy: LaunchSafetyPolicy
     ) {
         self.title = title
         self.subtitle = subtitle
         self.statusMessage = statusMessage
         self.configuredSourceCount = configuredSourceCount
+        self.sourceDiscoverySummary = sourceDiscoverySummary
+        self.refreshState = refreshState
         self.safetyPolicy = safetyPolicy
     }
 }
